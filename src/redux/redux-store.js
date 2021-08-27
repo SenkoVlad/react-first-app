4793609941d963653dbfdede631bb1477bc14517
SenkoVlad <vlad.senko2011@yandex.ru>
2021-08-27 16:28:38 +0300

    fix

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux'
 import profileReducer from './profile-reducer'
 import dialogReducer from './dialog-reducer'
 import sidebarReducer from './sidebar-reducer'
@@ -18,8 +18,9 @@ let reducers = combineReducers ({
     app : appReducer,
     form: formReducer
 });
-
-let store = createStore(reducers, applyMiddleware(thunkMiddleWare));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleWare)
+));
 
 window.store = store;
 
